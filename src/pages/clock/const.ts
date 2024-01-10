21d841f46266bbf546664d986e1957fffd895361
Jinho Kim <wlsghrla94@gmail.com>
2024-01-10 21:41:15 +0900

    Add hands dataset

---
+++ src/pages/clock/const.ts
@@ -0,0 +1,5 @@
+export const HANDS = {
+  HOUR: { width: '16', height: '170', backgroundColor: 'black' },
+  MINUTE: { width: '10', height: '280', backgroundColor: 'black' },
+  SECOND: { width: '6', height: '290', backgroundColor: 'red' }
+};
